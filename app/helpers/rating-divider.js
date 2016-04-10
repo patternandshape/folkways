d0d3636f11554dc303a38bb51d1c56b952ff04e4
Molly Curtin <patternandshape@gmail.com>
2016-04-10 14:28:15 -0700

    add styling to review and add release forms as well as the review field on each release page

--- app/helpers/rating-divider.js
+++ app/helpers/rating-divider.js
@@ -4,9 +4,9 @@ export function ratingDivider(params) {
   var reviewRating = params[0].get('rating');
 
   if (reviewRating >= 5){
-    return Ember.String.htmlSafe('<i class="fa fa-thumbs-o-up"></i>');
+    return Ember.String.htmlSafe('<i class="fa fa-thumbs-up"></i>');
   } else if (reviewRating < 5){
-    return Ember.String.htmlSafe('<i class="fa fa-thumbs-o-down"></i>');
+    return Ember.String.htmlSafe('<i class="fa fa-thumbs-down"></i>');
   }
 }
 
