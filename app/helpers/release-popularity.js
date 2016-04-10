9c722dc22fd1a1281d73fe5b3dc19f7bb7b7cb2d
Molly Curtin <patternandshape@gmail.com>
2016-04-10 09:11:23 -0700

    add release-popularity filter which shows an icon for releases that have many reviews

---
+++ app/helpers/release-popularity.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export function releasePopularity(params) {
+  var release = params[0];
+
+  if(release.get('reviews').get('length') >= 1) {
+    return Ember.String.htmlSafe('<i class="fa fa-commenting-o"></i>');
+  }
+}
+
+export default Ember.Helper.helper(releasePopularity);
---
+++ tests/unit/helpers/release-popularity-test.js
@@ -0,0 +1,10 @@
+import { releasePopularity } from 'folkways/helpers/release-popularity';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | release popularity');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = releasePopularity([42]);
+  assert.ok(result);
+});
