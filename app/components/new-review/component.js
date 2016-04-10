20338a997ab16d24cd2e3d33a3e72ee441f854d4
Molly Curtin <patternandshape@gmail.com>
2016-04-09 18:13:04 -0700

    add parseint to rating parameter in new-review component

--- app/components/new-review/component.js
+++ app/components/new-review/component.js
@@ -10,11 +10,15 @@ export default Ember.Component.extend({
       var params = {
         author: this.get('author'),
         date: Date.now(),
-        // rating: this.get('rating'),
+        rating: parseInt(this.get('rating')),
         body: this.get('body'),
         release: this.get('release')
       };
       this.set('addNewReview', false);
+      this.set('author', "");
+      this.set('date', "");
+      this.set('rating', "");
+      this.set('body', "");
       this.sendAction('saveReview2', params);
     }
   }
--- app/review/model.js
+++ app/review/model.js
@@ -3,7 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   author: DS.attr(),
   date: DS.attr(),
-  // rating: DS.attr(),
+  rating: DS.attr(),
   body: DS.attr(),
   release: DS.belongsTo('release', {async: true})
 });
